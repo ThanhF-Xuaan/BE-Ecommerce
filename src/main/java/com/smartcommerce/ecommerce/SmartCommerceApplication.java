6c5b699c4190635798a1c13ff4b7e9c9f9d94acb
ThanhF-Xuaan <le227460@gmail.com>
2025-08-25 07:05:30 +0700

    init project

---
+++ src/main/java/com/smartcommerce/ecommerce/SmartCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.smartcommerce.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCommerceApplication.class, args);
+    }
+
+}
